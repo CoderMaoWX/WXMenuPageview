ac952bf7244f221d57528db1af5574cc68c590b7
luocheng <maowangxin_2013@163.com>
2019-12-31 00:17:42 +0800

    测试头部为表格时的滚动

--- WXMenuPageView/PageHeaderView.m
+++ WXMenuPageView/PageHeaderView.m
@@ -9,17 +9,19 @@
 #import "PageHeaderView.h"
 #import "Header.h"
 
-@interface PageHeaderView ()
+@interface PageHeaderView ()<UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView       *tableView;
 @property (nonatomic, strong) UIImageView       *headerImageView;
 @property (nonatomic, strong) UIButton          *tmpMenuBtn;
 @end
 
 @implementation PageHeaderView
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
+- (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+//        [self addSubview:self.tableView];
+        
         [self addSubview:self.headerImageView];
         self.tmpMenuBtn = [self createButton:0];
         self.tmpMenuBtn.selected = YES;
@@ -27,6 +29,38 @@
         [self addSubview:[self createButton:1]];
     }
     return self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class])];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.contentView.backgroundColor = kRandomColor;
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"didSelectRowAtIndexPath==%ld", indexPath.row);
+}
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.bounds style:UITableViewStylePlain];
+        _tableView.backgroundColor = [UIColor whiteColor];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.rowHeight = 80;
+        _tableView.tableFooterView = [UIView new];
+        _tableView.exclusiveTouch = YES;
+        _tableView.tableFooterView = self.headerImageView;
+        _tableView.scrollEnabled = NO;
+        
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
+    }
+    return _tableView;
 }
 
 - (UIImageView *)headerImageView {
